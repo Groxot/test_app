de42032cadea1af8365e517e63b5c952339632d6
Groxot <w8it@mail.com>
2015-06-05 15:44:07 +0300

    initial

---
+++ js/test1contr.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('test1',['restangular']);
+
+
+ app.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('https://api.github.com/');
+});
+
+app.controller('test1contr', function($scope, Restangular) {
+	$scope.filterText = [];
+	$scope.filterText.login = '';
+	var gitUsers = Restangular.all('users');
+	gitUsers.getList().then(function(users) {
+	  $scope.allUsers = users;
+	})
+});
