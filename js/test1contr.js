9c9acdacab757a2de20c99f0792f001c8d55c389
Gopard <w8it@mail.com>
2015-06-05 20:42:48 -0500

    Update for the task

--- js/test1contr.js
+++ js/test1contr.js
@@ -1,15 +1,62 @@
-﻿var app = angular.module('test1',['restangular']);
+﻿var app = angular.module('test1',['restangular','ngMessages','ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider, RestangularProvider) {
+ RestangularProvider.setBaseUrl('https://api.github.com/');
+  $urlRouterProvider.otherwise("/");
 
- app.config(function(RestangularProvider) {
-  RestangularProvider.setBaseUrl('https://api.github.com/');
+  $stateProvider
+    .state('test1', {
+      url: "/test1",
+      templateUrl: "test1.html",
+	  resolve:{
+         allUsers:  function(Restangular){
+			return Restangular.all('users').getList().then(function(users) {
+			  return users; 
+			})       
+		 }
+	   },
+	    controller:"test1contr"
+	})
+    .state('test2', {
+      url: "/test2",
+      templateUrl: "test2.html",
+	  controller:"test2contr",
+    })
 });
 
-app.controller('test1contr', function($scope, Restangular) {
+app.controller('test1contr', ['$scope', 'allUsers', function($scope, allUsers) {
+	$scope.allUsers=allUsers;
 	$scope.filterText = [];
 	$scope.filterText.login = '';
-	var gitUsers = Restangular.all('users');
-	gitUsers.getList().then(function(users) {
-	  $scope.allUsers = users;
-	})
+
+}]);
+app.controller('test2contr', function($scope) {
+	 $scope.user = {
+		email: "",
+		password: "",
+		repeat_password: ""
+    };
+	$scope.passwordLen=8;
+
+
 });
+var compareTo = function() {
+	return {
+		require: "ngModel",
+			scope: {
+					otherModelValue: "=compareTo"
+				},
+			link: function(scope, element, attributes, ngModel) {
+
+				ngModel.$validators.compareTo = function(modelValue) {
+						return modelValue == scope.otherModelValue;
+					};
+
+				scope.$watch("otherModelValue", function() {
+						ngModel.$validate();
+					});
+				}
+	};
+};
+
+app.directive("compareTo", compareTo);
